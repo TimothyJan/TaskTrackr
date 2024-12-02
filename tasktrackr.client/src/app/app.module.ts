b1b743d030ea3f630277030a33ec778f34f7d49f
Timothy jan <jan.timothy@gmail.com>
2024-12-02 10:45:00 -0800

    Footer and Header Complete

--- tasktrackr.client/src/app/app.module.ts
+++ tasktrackr.client/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserModalComponent } from './components/user-list/user-modal/user-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserListComponent,
+    UserModalComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
